b938845a642a60086467624603d8e9275247df88
Fₗₒw ᵢₙ ₜₕₑ dₐᵣₖₙₑₛₛ <134411637+BenjaminGajardoRo@users.noreply.github.com>
2024-08-22 09:16:31 -0400

    Update create_hash.py

--- create_hash.py
+++ create_hash.py
@@ -1,6 +1,4 @@
-# create_hash.py
 from werkzeug.security import generate_password_hash
 
-# Genera el hash de la contraseña
 hashed_password = generate_password_hash('admin', method='pbkdf2:sha256')
 print(hashed_password)
